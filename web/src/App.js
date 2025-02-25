bfb02b8617c7e1edd39e925126fd1a1aab623678
Sant-Thiago <thiagovieirab2b@gmail.com>
2025-02-25 15:43:44 -0300

    Uploading project react

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" components={''} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
